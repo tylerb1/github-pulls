eab2dffefcb4b3b30411e41ba86cf6ec0b5fecf7
tylerb1 <tberb1290@gmail.com>
2022-07-06 11:18:19 -0500

    get all pages of all types of PRs for each repio

--- build/src/index.js
+++ build/src/index.js
@@ -2,8 +2,7 @@ import fetch from "node-fetch";
 import cache from "node-cache";
 const pullsByRepo = {};
 const prCache = new cache();
-const githubToken = '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';
-let nPulls = 0;
+const githubToken = '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'; // Enter personal access token here
 const reposResponse = await fetch('https://api.github.com/orgs/ramda/repos', {
     headers: {
         'Accept': 'application/vnd.github.v3+json',
@@ -12,18 +11,21 @@ const reposResponse = await fetch('https://api.github.com/orgs/ramda/repos', {
 });
 const repos = await reposResponse.json();
 for (const repo of repos) {
-    const pullsResponse = await fetch(`https://api.github.com/repos/ramda/${repo.name}/pulls`, {
-        headers: {
-            'Accept': 'application/vnd.github.v3+json',
-            'Authorization': `Bearer ${githubToken}`,
-        }
-    });
-    const pulls = await pullsResponse.json();
-    nPulls += pulls.length;
-    pullsByRepo[repo.name] = pulls;
+    let nResults = 0, pageNumber = 1;
+    pullsByRepo[repo.name] = [];
+    while (nResults < 100) {
+        const pullsResponse = await fetch(`https://api.github.com/repos/ramda/${repo.name}/pulls?state=all&per_page=100&page=${pageNumber}`, {
+            headers: {
+                'Accept': 'application/vnd.github.v3+json',
+                'Authorization': `Bearer ${githubToken}`,
+            }
+        });
+        const pulls = await pullsResponse.json();
+        pullsByRepo[repo.name] = pullsByRepo[repo.name].concat(pulls);
+        nResults = pulls.length;
+        pageNumber += 1;
+    }
 }
-const success = prCache.set(`prs`, pullsByRepo, 10000);
-const prs = prCache.get(`prs`);
-console.log(prs);
-console.log(nPulls);
+console.log(pullsByRepo);
+prCache.set(`pulls`, pullsByRepo, 10000);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -13,13 +13,23 @@ const reposResponse = await fetch('https://api.github.com/orgs/ramda/repos', {
 const repos = await reposResponse.json();
 
 for (const repo of repos) {
-  const pullsResponse = await fetch(`https://api.github.com/repos/ramda/${repo.name}/pulls`, { 
-    headers: {
-      'Accept' : 'application/vnd.github.v3+json',
-      'Authorization': `Bearer ${githubToken}`,
-    }});
-  const pulls = await pullsResponse.json();
-  pullsByRepo[repo.name] = pulls;
+  let pageNumber = 1;
+  let pullsForRepo: any[] = [];
+  let pulls: any[] = [];
+  while (pulls.length === 100 || pageNumber === 1) {
+    const pullsResponse = await fetch(
+      `https://api.github.com/repos/ramda/${repo.name}/pulls?state=all&per_page=100&page=${pageNumber}`, 
+      { 
+        headers: {
+          'Accept' : 'application/vnd.github.v3+json',
+          'Authorization': `Bearer ${githubToken}`,
+        }
+      });
+    pulls = await pullsResponse.json();
+    pullsForRepo.push(...pulls);
+    pageNumber += 1;
+  }
+  pullsByRepo[repo.name] = pullsForRepo;
 }
 
 prCache.set(`pulls`, pullsByRepo, 10000);
